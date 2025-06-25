bdd583ae725cfd1bde09b8e40b56062a0b05e059
Trieu Nguyen <trieu.nguyen@sioux.asia>
2025-06-25 17:36:50 +0700

    Add a button

---
+++ Circle_Drawing/ViewModels/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using Circle_Drawing.Commands;
+
+namespace Circle_Drawing.ViewModels
+{
+    public class MainViewModel
+    {
+        public ICommand ShowMessageCommand { get; }
+
+        public MainViewModel()
+        {
+            ShowMessageCommand = new RelayCommand(_ => ShowMessage());
+        }
+
+        private void ShowMessage()
+        {
+            MessageBox.Show("You clicked the button!");
+        }
+    }
+}
---
+++ Circle_Drawing/Views/MainView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+using Circle_Drawing.ViewModels;
+
+namespace Circle_Drawing.Views
+{
+    public partial class MainView : UserControl
+    {
+        public MainView()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+    }
+}
