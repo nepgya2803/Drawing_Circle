b1ecf1ee2b5b73efd5e4333feb90f2cd638aa41f
Trieu Nguyen <trieu.nguyen@sioux.asia>
2025-06-27 17:14:20 +0700

    Add a button

--- Circle_Drawing/App.xaml.cs
+++ Circle_Drawing/App.xaml.cs
@@ -7,6 +7,18 @@ namespace Circle_Drawing
     /// </summary>
     public partial class App : Application
     {
-    }
+        private void Startup_Show(object sender, StartupEventArgs ev)
+        {
+            if (OperatingSystem.IsLinux()) throw new Exception("Os is not compatible");
 
+            var wnd = new MainWindow
+            {
+                Title = "Circle Drawing",
+                Height = 800,
+                Width = 600
+            };
+
+            wnd.Show();
+        }
+    }
 }
--- Circle_Drawing/Commands/ReplayCommand.cs
+++ Circle_Drawing/Commands/ReplayCommand.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Windows.Input;
+﻿using System.Windows.Input;
 
 namespace Circle_Drawing.Commands
 {
     public class RelayCommand : ICommand
     {
-        private readonly Action<object?> _execute;
         private readonly Predicate<object?>? _canExecute;
+        private readonly Action<object?> _execute;
 
         public RelayCommand(Action<object?> execute, Predicate<object?>? canExecute = null)
         {
@@ -16,12 +15,18 @@ namespace Circle_Drawing.Commands
 
         public event EventHandler? CanExecuteChanged
         {
-            add { CommandManager.RequerySuggested += value!; }
-            remove { CommandManager.RequerySuggested -= value!; }
+            add => CommandManager.RequerySuggested += value!;
+            remove => CommandManager.RequerySuggested -= value!;
         }
 
-        public bool CanExecute(object? parameter) => _canExecute?.Invoke(parameter) ?? true;
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute?.Invoke(parameter) ?? true;
+        }
 
-        public void Execute(object? parameter) => _execute(parameter);
+        public void Execute(object? parameter)
+        {
+            _execute(parameter);
+        }
     }
 }
